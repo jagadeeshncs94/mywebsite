44ca01c5de3a31b1c7578544cd3b688d888009eb
jagadeeshncs94 <92731763+jagadeeshncs94@users.noreply.github.com>
2021-10-18 18:47:46 +0530

    adding this file

---
+++ mypythonfiles/list_comp2.py
@@ -0,0 +1,33 @@
+result =[l for l in "python"]
+print(result)
+
+result1=[h for h in "jagadeesh"]
+print(result1)
+
+result2=[i for i in ["datatype","result","datamine","types"]]
+print(result2)
+
+case=[{r:1+2} for r in [10,11,13,11]]
+print(case)
+print(type(case[0]))
+
+
+#case2=[o for o in [12,13,14,18]]
+#print(case2)
+
+#example
+numbers=[12,15,47,78]
+for n in numbers:
+    print(n+12)
+
+tables=[14,87,98,55]
+for l in range(len(tables)):
+    print(tables[l])
+
+a=(12,15,85,54)
+for b in a:
+    print(b+2)
+
+
+
+
